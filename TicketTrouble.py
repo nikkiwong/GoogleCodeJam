87739e3243f2c1d11795be0b7aa0f16211aba5f2
Nikki Wong <nikki.wong@ucdconnect.ie>
2017-03-12 01:26:34 +0000

    After 8 hours, I have finally completed the code for question 1 part a. Now to tidy it..

--- TicketTrouble.py
+++ TicketTrouble.py
@@ -1,40 +1,22 @@
-import argparse
-import urllib.request
-import numpy as np
-
+import operator
 
 def parse(buffer):
 
     line = buffer.split('\n')
-    newfile = ()
+    #putting all the data into a list of lists
+    newfile = []
     for x in line:
         values = x.strip().split()
         newfile += (values,)
-    # print(newfile)
-    # print(" ")
-    # print(newfile[0], newfile[1][1])
     return newfile
 
-def f6(seq):
-   # Not order preserving
-   set = Set(seq)
-   return list(set)
-
 def solution(file):
-    # file = [(x,) for x in file]
-    # print(file)
-    if type(file) is list:
-        print("a list")
-    elif type(file) is tuple:
-        print('a tuple')
-    else:
-        print('neither a tuple or a list')
+
     f = file[0]
     tot = int(f[0])
-    print(tot)
     d = {}
     rows = []
-    same = []
+    result = []
     rowCount = 0
     sameCount = 0
     a=1
@@ -42,62 +24,50 @@ def solution(file):
     F=0
     while x<tot+1:
         d["Case #{0}".format(x)] = 0
+        #this condition is because you don't want to change the value of a in the very first run
         if a<len(file):
             if F!=0:
                 a+=F+1
             F = int(file[a][0])
-            # frnds = F
             S = int(file[a][1])
-            w = a
             print("F: ", F, "S: ", S)
-            print("w",w,"+1: ", w+1, (w+F))
+            print("w",a,"+1: ", a+1, (a+F))
 
             for n in range(1, S+1):
+            #from the number 1 --> max grid size. Iterates through all the numbers that could possibly be on the ticket
+            #splitting the section that containing the number of relevant tickets
+                section = file[a + 1: a + F + 1]
+                print("section: ", section)
+                #because the section is a list in a list I used this way to remove duplicates in 'section'. Removing duplicates because if there are duplicates
+                #it means that there is no chance that the friends will sit in the same row.
+                b_set = set(tuple(x) for x in section)
+                b = [list(x) for x in b_set]
+                print("section: ", b)
 
-                # for i in range(w+1, (w+F+1)):
-                section = file[w + 1: w + F + 1]
-                print("section: ", section)
-                myList = sorted(set(section))
-                print("section: ", section)
-                for i in section:
-                    # print(i)
-                    # print(i[0])
+                for i in b:
+                # for every ticket that each friend has in this section
                     print("are the numbers of line",i,":", "equal to ", n)
-
+                    #there is a max number of rows/columns so that would be the max number on the ticket (x, y), therefore I'm iterating through
+                    #each ticket to see if x or y is equal to the numbers from 1 --> max number one by one.. if the number appears it gets appended to
+                    #the list 'rows'.
                     if int(i[0])== n or int(i[1]) == n:
                         rowCount+=1
                         print("rowcount", rowCount)
-
-
-                    # if int(file[i][1]) == n:
-                    #     colCount+=1
-                    #     print("colCount", colCount)
-
-            # x += 1
                 rows.append(rowCount)
-                same.append(sameCount)
-                print("rows: ", rows,"same:", same)
+                print("rows: ", rows,"tots:", result)
                 rowCount = 0
-                # colCount = 0
-            # for i in range(1, S):
-            #     if rows[i]>rows[i+1]:
-            #         rMax = rows[i]
-            #     else:
+                # the counter is reset every time we check for a new number to match with the ticket.
+            result.append(max(rows))
+            #we take the max number from the list rows that's from this section and put it into a new list called 'tots'.
+            rows = []
+            #reset the list for the next section
 
-            rMax = max(rows)
-                # if cols[i] > cols[i+1]:
-                #     cMax = cols[i]
-                # else:
-            cMax = max(same)
-            if cMax>rMax:
-                d["Case #{0}".format(x)] = cMax
-            else:
-                d["Case #{0}".format(x)] = rMax
-
-
-
+            d["Case #{0}".format(x)] = result[x-1]
         x+=1
-    return d
+
+    x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+    sorted_d = sorted(d.items(), key=operator.itemgetter(0))
+    return sorted_d
 
 
 
